cbbb92dfdd21a8ee8490c9e1bdd7e31bcd689274
Rafael Caballero <rafaelcaballeromora@gmail.com>
2023-06-21 22:26:26 +0100

    remove ng-select and fix package lock

--- src/generic-components/generic-components.module.ts
+++ src/generic-components/generic-components.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DropdownComponent } from './dropdown/dropdown.component';
-import { NgSelectModule } from '@ng-select/ng-select';
 import { InfoIconComponent } from './info-icon/info-icon.component';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   declarations: [DropdownComponent, InfoIconComponent],
-  imports: [CommonModule, NgSelectModule, MatTooltipModule, MatSelectModule],
+  imports: [CommonModule, MatTooltipModule, MatSelectModule],
   exports: [DropdownComponent],
 })
 export class GenericComponentsModule {}
