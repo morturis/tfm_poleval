84d55e175fd71cac291ed4236d19a03410f2f3ff
Rafael Caballero <rafaelcaballeromora@gmail.com>
2023-06-30 09:29:34 +0100

    toolbar component

--- src/generic-components/toolbar/toolbar.component.ts
+++ src/generic-components/toolbar/toolbar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { TranslationService } from 'src/services/translation-service.service';
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
-  styleUrls: ['./toolbar.component.scss']
+  styleUrls: ['./toolbar.component.scss'],
 })
 export class ToolbarComponent {
-
+  constructor(public ts: TranslationService) {}
 }
--- src/languages/enums/PhraseKey.enum.ts
+++ src/languages/enums/PhraseKey.enum.ts
@@ -4,4 +4,10 @@ export enum PhraseKey {
   button_remove_last = 'button_remove_last',
   button_cancel = 'button_cancel',
   button_ok = 'button_ok',
+  toolbar_home = 'toolbar_home',
+  toolbar_contact = 'toolbar_contact',
+  toolbar_we_are = 'toolbar_we_are',
+  toolbar_log_in = 'toolbar_log_in',
+  toolbar_sign_up = 'toolbar_sign_up',
+  new_eval = 'new_eval',
 }
